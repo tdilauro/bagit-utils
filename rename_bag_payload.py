8a19ad037d96d99dbf98b5af7a20552e9d5d1b35
Tim DiLauro <tim.di.lauro+gitkraken@gmail.com>
2019-02-15 01:19:04 -0500

    don't validate unless there are files to rename. more status output.

--- rename_bag_payload.py
+++ rename_bag_payload.py
@@ -26,30 +26,48 @@ def main():
     processes = args.processes
 
     for bag_dir in args.directories:
+        print("*** Starting processing of bag in directory '%s'" % bag_dir)
         bag = Bag(bag_dir)
-        bag_short_name = os.path.basename(bag.path)
-        bag.validate(processes=processes,)
+        bag_name = os.path.basename(bag.path)
 
         # our renamer is a generator
         payload_files = sorted(bag.payload_files())
-        rename_map = OrderedDict((old, new) for old, new
-                                 in rename_files(payload_files, basedir=bag.path, dry_run=args.dry_run))
+        rename_map = OrderedDict((old, new) for old, new in rename_files(payload_files, basedir=bag.path))
+        rename_count = len(rename_map)
+        # if no entries in rename_map, then we are not remapping, so won't need to perform validation
+        remapping = (rename_count > 0)
 
         if args.map or args.map_file is not None:
             if args.map_file is not None:
                 map_file = args.map_file
             else:
-                map_file = 'renameLog-' + bag_short_name + '-' + datetime.now().strftime('%Y%m%dT%H%M%S') + '.csv'
+                map_file = 'renameLog-' + bag_name + '-' + datetime.now().strftime('%Y%m%dT%H%M%S') + '.csv'
             print("Printing rename map to file '%s'" % map_file)
             emit_rename_map(rename_map, filename=map_file, type='csv')
 
-        if not args.dry_run:
-            # update the manifests
+        if not remapping:
+            print("No files to rename. No updates or bag validations will be performed.")
+        elif not args.dry_run:
+            # run pre-update validation to ensure that bag is okay before we start
+            print("Pre-update validation of bag '%s'..." % bag_name, end='')
+            bag.validate(processes=processes,)
+            print('finished.')
+
+            print('Renaming %d files in the filesystem...' % rename_count, end='')
+            success = all([fs_rename(old, new, basedir=bag.path, dry_run=False) for old, new in rename_map.items()])
+            print('finished.')
+            print("Updating bag '%s' payload and tag manifests..." % bag_name, end='')
             bag.update_payload_filenames(rename_map=rename_map)
+            print('finished.')
 
             # re-open and validate the update bag
             bag = bag.refresh()
+            print("Post-update validation of bag '%s'..." % bag_name, end='')
             bag.validate(processes=processes, )
+            print('finished.')
+
+        print("... Completed processing of bag in directory '%s'" % bag_dir)
+
 
 
 def emit_rename_map(rename_map, filename=None, type='csv'):
@@ -60,7 +78,7 @@ def emit_rename_map(rename_map, filename=None, type='csv'):
             writer.writerow([old, new])
 
 
-def rename_files(files_to_rename, basedir='', institution='jhu', interfield_sep='_', intrafield_sep='-', dry_run=False):
+def rename_files(files_to_rename, basedir='', institution='jhu', interfield_sep='_', intrafield_sep='-'):
     previously = successes = failures = 0
     collection = intrafield_sep.join(os.path.basename(basedir).split(intrafield_sep)[0:2])
     for filepath in files_to_rename:
@@ -72,7 +90,7 @@ def rename_files(files_to_rename, basedir='', institution='jhu', interfield_sep=
             dir_tree[-1] = new_basename
             new_filepath = os.sep.join(dir_tree)
             # print("- renaming '%s' to '%s'..." % (filepath, new_filepath), end='')
-            if fs_rename(filepath, new_filepath, basedir=basedir, dry_run=dry_run):
+            if fs_rename(filepath, new_filepath, basedir=basedir, dry_run=True):
                 successes += 1
                 # print('success.')
                 # yield the old and new path only on success
@@ -80,11 +98,11 @@ def rename_files(files_to_rename, basedir='', institution='jhu', interfield_sep=
             else:
                 failures += 1
                 # print('failure.')
-    print('summary filesystem renaming: successes: %d, failures: %d, previously renamed: %d'
+    print('Renaming plan summary: To be renamed: %d; Cannot rename: %d; Previously renamed: %d'
           % (successes, failures, previously))
 
 
-def fs_rename(old, new, basedir='', dry_run=False):
+def fs_rename(old, new, basedir='', dry_run=True):
     old = os.path.join(basedir, old)
     new = os.path.join(basedir, new)
     if old != new and os.path.exists(old) and not os.path.exists(new):
